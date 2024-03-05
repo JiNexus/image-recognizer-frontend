ee40e9744c93b8b27bac79f22be8ead824ddcd1e
JiNexus <jinvirle@gmail.com>
2024-03-06 00:02:40 +0800

    Clean App.vue and add new route for image recognizer

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
+import ImageRecognizerVue from '@/views/ImageRecognizerView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: HomeView,
         },
         {
             path: '/about',
@@ -15,7 +16,12 @@ const router = createRouter({
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue')
+            component: () => import('../views/AboutView.vue'),
+        },
+        {
+            path: '/image-recognizer',
+            name: 'image-recognizer',
+            component: ImageRecognizerVue,
         }
     ],
 });
