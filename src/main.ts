02a6c1d65aba308d63483186cc15fae2baa18508
JiNexus <jinvirle@gmail.com>
2024-03-06 00:01:26 +0800

    Import bootstrap and fontawesome to the app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-import './assets/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from './App.vue';
-import router from './router';
+import App from '@/App.vue';
+import router from '@/router';
+
+/* add icons to the library */
+import { faFileImport } from '@fortawesome/free-solid-svg-icons';
+import { faBoltLightning } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faFileImport, faBoltLightning);
 
 const app = createApp(App);
 
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia());
 app.use(router);
 
