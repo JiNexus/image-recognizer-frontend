47817ce6a364837328e65e120e0e1558ed9c3bb6
JiNexus <jinvirle@gmail.com>
2024-03-10 22:38:42 +0800

    Add p5js as CDN for drawing functionality and ml5js as CDN for friendly machine learning using Web

--- src/views/ImageRecognizerView.vue
+++ src/views/ImageRecognizerView.vue
@@ -4,6 +4,19 @@ import ImageRecognizer from '@/components/ImageRecognizer.vue';
 export default {
     name: 'ImageRecognizerView',
     components: { ImageRecognizer },
+    mounted() {
+        const scriptP5 = document.createElement('script');
+        scriptP5.src = 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.1/p5.min.js';
+        scriptP5.async = true;
+
+        document.head.appendChild(scriptP5);
+
+        const scriptMl5 = document.createElement('script');
+        scriptMl5.src = 'https://unpkg.com/ml5@0.12.2/dist/ml5.min.js';
+        scriptMl5.async = true;
+
+        document.head.appendChild(scriptMl5);
+    },
 }
 </script>
 
